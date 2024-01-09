83609b95c025733dce8abe50173300baefd16ff5
Jacob Foote <jacobjfoote01@gmail.com>
2024-01-08 21:30:24 -0700

    added a Resume link

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,34 @@
+import React, { useState } from "react";
 import "../css/navbar.css";
 import Logo from "./Logo";
 
+const ResumeDropdown = () => {
+  const [dropdown, setDropdown] = useState(false);
+
+  const toggleDropdown = () => {
+    setDropdown(!dropdown);
+  };
+  return (
+    <div className="dropdown">
+      <a className="navResume" onClick={toggleDropdown}>
+        Resume
+      </a>
+      {dropdown && (
+        <div className="dropdown-content">
+          <a href="">PDF</a>
+          <br />
+          <a href="">Word</a>
+          <br />
+          <a href="">HTML</a>
+        </div>
+      )}
+    </div>
+  );
+};
+
 const NavBar = () => {
   return (
-    <>
+    <nav>
       <a href="/" className="navHome">
         <Logo />
       </a>
@@ -16,8 +41,8 @@ const NavBar = () => {
         Contact
       </a>
       <br />
-      <a className="navResume">Resume</a>
-    </>
+      <ResumeDropdown className="navResume" />
+    </nav>
   );
 };
 
