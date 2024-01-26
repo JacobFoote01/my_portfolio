f01cc7a73957352c2e4a73f1370de6f006c6ab7a
Jacob Foote <jacobjfoote01@gmail.com>
2024-01-26 13:09:31 -0700

    added a few things. mainly a scroll bar for the projects.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,13 +5,16 @@ const About = () => {
     <>
       <div className="about">
         <h1 className="aboutTitle">About Me</h1>
+        <hr id="line" />
         <img className="headShot" src="Jacob Headshot.jpg" />
+        <hr id="line" />
         <div className="textBody">
           Hello! My name is Jacob Foote. I am a full-stack web developer who
-          loves learning new things. I completed the Web Development Program at
-          Devmountain in December of 2023. When I am not coding, you can find me
-          working on cars, snowboarding, biking, or usually spending time with
-          my wife and son.
+          loves learning new things. I am 27 years old and the sixth of seven
+          children. I was born and raised In Salt Lake City, Utah. I completed
+          the Web Development Program at Devmountain in December of 2023. When I
+          am not coding, you can find me working on cars, snowboarding, biking,
+          or usually spending time with my family.
         </div>
       </div>
     </>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,9 @@ import { FaFolderOpen } from "react-icons/fa";
 import "../css/projects.css";
 
 const Projects = () => {
+  const handleClick = (url) => {
+    window.open(url, "_blank");
+  };
   return (
     <div className="projectsContainer">
       <h1 className="projectsTitle">
@@ -10,14 +13,24 @@ const Projects = () => {
       </h1>
       <div className="projectBox">
         <h3 className="projectTitle">Checkr</h3>
-
-        <button className="github">Github</button>
-        <button className="demo">Demo</button>
-        <div>Skills:</div>
-        <div>
+        <div className="buttonCont">
+          <button
+            className="github"
+            onClick={() =>
+              handleClick("https://github.com/paulG1218/DM-Project3.git")
+            }
+          >
+            Github
+          </button>
+        </div>
+        <hr id="line" />
+        <div className="skillsUsed">Skills:</div>
+        <div className="skillList">
           React, Javascript, Html5, Css, Node.js, PostgreSQL, Redux, Axios
         </div>
-        <div>
+        <hr id="line" />
+        <div className="skillsUsed">Description:</div>
+        <div className="description">
           A collaborative 'To-Do List' application integrates your personal and
           professional tasks. Each list is filled with rewarding activities
           completing tasks earns you points, accompanied by delightful
@@ -27,16 +40,76 @@ const Projects = () => {
       </div>
       <div className="projectBox">
         <h3 className="projectTitle">Vehicle Log</h3>
-        <button className="github">Github</button>
-        <button className="demo">Demo</button>
-        <div>Skills:</div>
-        <div>React, Javascript, Html5, Css, Node.js, PostgreSQL, Axios</div>
-        <div>
+        <div className="buttonCont">
+          <button
+            className="github"
+            onClick={() =>
+              handleClick("https://github.com/JacobFoote01/Project-2.git")
+            }
+          >
+            Github
+          </button>
+        </div>
+        <hr id="line" />
+        <div className="skillsUsed">Skills:</div>
+        <div className="skillList">
+          React, Javascript, Html5, Css, Node.js, PostgreSQL, Axios
+        </div>
+        <hr id="line" />
+        <div className="skillsUsed">Description:</div>
+        <div className="description">
           A React application tailored for car enthusiasts. Easily log in or
           create an account, manage vehicle details, create new entries, and
           track modifications and maintenance history effortlessly. The app
           ensures a secure experience with authentication, user-friendly vehicle
           creation, and convenient logout functionality
+        </div>
+      </div>
+      <div className="projectBox">
+        <h3 className="projectTitle">Task List</h3>
+        <div className="buttonCont">
+          <button
+            className="github"
+            onClick={() =>
+              handleClick("https://github.com/JacobFoote01/task_list.git")
+            }
+          >
+            Github
+          </button>
+        </div>
+        <hr id="line" />
+        <div className="skillsUsed">Skills:</div>
+        <div className="skillList">React, Javascript, Html5, Css, Node.js </div>
+        <hr id="line" />
+        <div className="skillsUsed">Description:</div>
+        <div className="description">
+          A "to-do list" style application. Has all CRUDE capabilities. Making
+          and organizing tasks such a breeze. Included an estimated time input
+          to further enhance the experience, providing a user-friendly interface
+          for efficient task management
+        </div>
+      </div>
+      <div className="projectBox">
+        <h3 className="projectTitle">Movie Ratings</h3>
+        <div className="buttonCont">
+          <button
+            className="github"
+            onClick={() =>
+              handleClick("https://github.com/JacobFoote01/MovieRatings1")
+            }
+          >
+            Github
+          </button>
+        </div>
+        <hr id="line" />
+        <div className="skillsUsed">Skills:</div>
+        <div className="skillList">React, Javascript, Html5, Css, Node.js</div>
+        <hr id="line" />
+        <div className="skillsUsed">Description:</div>
+        <div className="description">
+          A collaborative React application that allow you to choose a movie and
+          give it a rating. Choose from your favorite movies and go give them
+          ratings so all of your friends can see what you thought.
         </div>
       </div>
     </div>
