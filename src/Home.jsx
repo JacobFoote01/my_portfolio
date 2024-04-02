2f1e2302e56bc79df4a8cc8f43bd2f57f0c79c3f
Jacob Foote <jacobjfoote01@gmail.com>
2024-04-01 21:22:19 -0600

    made a few changes to get the app to render locally

--- src/Home.jsx
+++ src/Home.jsx
@@ -21,6 +21,7 @@ const Home = () => {
 
   return (
     <div className="homeContainer">
+      <h1>Hello</h1>
       <div className="buttonContainer">
         <button className="contact" onClick={handleContact}>
           <MdOutlineEmail /> Contact Me
