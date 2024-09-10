95ea9fea411992278aeb3ba87ebe7540ddd5fed5
Jacob Foote <jacobjfoote01@gmail.com>
2024-09-10 13:58:00 -0600

    added to the about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
           Hello! My name is Jacob Foote. I am a full-stack web developer who
           loves learning new things. I completed the Web Development Program at
           Devmountain in December of 2023. When I am not coding, you can find me
-          working on cars, snowboarding, biking, or usually spending time with
+          working on cars, snowboarding, biking, camping, or usually spending time with
           my wife and son.
         </div>
       </div>
