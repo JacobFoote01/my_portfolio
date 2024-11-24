e5b869bfa26132f48d4607188f2bab342450880b
Jacob Foote <jacobjfoote01@gmail.com>
2024-11-24 16:16:11 -0700

    added a few more projects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,6 +4,18 @@ import "../css/projects.css";
 
 const projects = [
   {
+    title: "DC Landworks",
+    github: "https://github.com/JacobFoote01/DC_Landworks",
+    skills: "React, Javascript, Html5, Css",
+    description: `This is a website for a landscaping company.`,
+  },
+  {
+    title: "Mountain Skies Construction",
+    github: "https://www.mountainskiesconstruction.com",
+    skills: "Css, Wordpress",
+    description: `This is a website for a construction company. I was able to make it multiple pages and flow really well from pictures to their story.`,
+  },
+  {
     title: "Checkr",
     github: "https://github.com/paulG1218/DM-Project3.git",
     skills: "React, Javascript, Html5, Css, Node.js, PostgreSQL, Redux, Axios",
